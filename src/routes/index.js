d341bdbf7451b232ac3a306be984bbf5077a978e
Alex couppoussamy <alex.couppoussamy@gmail.com>
2018-11-16 10:39:09 -0500

    init serveur corrigé

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const cors= require('cors');
 const Partie = require('../modeles/partie.js');
 const Joueur = require('../modeles/joueur.js');
 
