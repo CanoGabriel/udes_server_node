a97dc592a31f161f5db468112d4c6319ec6a870c
Alex couppoussamy <alex.couppoussamy@gmail.com>
2018-11-16 10:30:30 -0500

    init serveur

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,12 @@ const Partie = require('../modeles/partie.js');
 const Joueur = require('../modeles/joueur.js');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/',cors({allowedHeaders:['Content-Type','Authorization']}), function (req, res, next) {
+
   const p = new Partie(new Joueur('Albert', 'Ramos', 28, 56, 'Espagne'), new Joueur('Milos', 'Raonic', 28, 16, 'Canada'), '1', 'hale', '12h30');
+  const p2= new Partie(new Joueur('All', 'Might', 32, 1, 'USA'), new Joueur('Midorya', 'Izuku', 16, 100, 'Japon'), '2', 'MHA', '14h30');
+  const tab=[p,p2];
   // res.send('Bienvenu dans le serveur du service Échanges.');
-  res.send(p);
+  res.send(tab);
 });
 module.exports = router;
